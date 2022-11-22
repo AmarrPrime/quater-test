446006f2b920d369068d0c6c587dc3d65c1b5af2
AmarrPrime <zloybirik@gmail.com>
2022-11-22 21:58:06 +0300

    program done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿/*Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма,
 обойтись массивами. */
+
+
+Console.WriteLine("Сколько элементов проверяем? ");
+int length = int.Parse(Console.ReadLine() ?? "");
+
+string[] strings = new string[length];
+
+string currentString = "";
+int count = 0;
+
+for (int i = 0; i < length; i++)
+{
+    currentString = Console.ReadLine();
+
+    if (currentString.Length <= 3)
+    {
+        strings[count] = currentString;
+        count++;
+    }
+}
+
+string[] resultStrings = new string[count];
+
+for (int i = 0; i < count; i++)
+{
+    resultStrings[i] = strings[i];
+}
+
+
+    Console.WriteLine("[{0}]", string.Join(", ", resultStrings));
